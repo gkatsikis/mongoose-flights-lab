d8b61124654e9d602ee429bcd9277db14f211090
gkatsikis <geo.bjj.kat@gmail.com>
2022-02-27 09:47:40 -0500

    connecting controller

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,8 +1,22 @@
 
+import res from 'express/lib/response'
+import { Flight } from '../models/flights.js'
+
 function newFlight(req, res) {
   res.render('flights/new')
 }
 
+function create(req, res) {
+   //what does this do and do I need it?
+}
+
+const flight = new Flight(req.body)
+flight.save(function(err) {
+  if (err) return res.redirect('/flights/new')
+  res.redirect('/flights/new')
+})
+
 export {
-  newFlight as new
+  newFlight as new,
+  create
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,7 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 router.get('/new', flightsCtrl.new)
 
+router.get('/', flightsCtrl.create) //what does this do and do I need it?
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource')
